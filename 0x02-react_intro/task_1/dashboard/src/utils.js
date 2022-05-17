7db4a443d275e0a6a93561925235c375b7d5ccd6
Orcha02 <orcha2002@gmail.com>
2022-05-17 15:36:14 -0500

    Create functions for footer

---
+++ 0x02-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,10 @@
+// export const getFullYear = () => new Date().getFullYear();
+export function getFullYear() {
+  var date = new Date();
+  return date.getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  if (isIndex) return "Holberton School";
+  return "Holberton School main dashboard";
+}
